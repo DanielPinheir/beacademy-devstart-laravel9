ec52c9e4fb9dcc44e550336a9dc85e9f0c51ca2c
DanielPinheir <dyelzim@gmail.com>
2022-07-01 20:01:30 -0300

    Update laravel class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,27 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = [
-            'nomes' => [
-                'José Lira',
-                'Daniel Pinheiro'
-            ]
-        ];
-        //dump e die no users
-        //vai le somente ate aqui
-        dd($users);
-    }
+        $users = User::all();
 
+        return view('users.index', compact('users'));
+    }
+    
     public function show($id)
-    {
-        $idUser = $id;
+    {   
+        //buscando pelo ID
 
-        \dd('Id do usuário é ' . $id);
+        if(!$user = User::find($id)) 
+            return redirect()->route('users.index');
+
+        $title = 'Usuário' . $user->name;
+
+        return view('users.show', compact('user', 'title'));
+
+        //$user = User::where('id', $id)->first();
     }
+
 }
